7fadfdad03f56ca0df622426d15a369e5c77d7bc
yexiaowei39 <22117002@zju.edu.cn>
2025-06-30 19:32:28 +0800

    Update fst&assoc.sh

--- 1_Identification/1_2-FST/fst&assoc.sh
+++ 1_Identification/1_2-FST/fst&assoc.sh
@@ -4,11 +4,10 @@ for chr in $(seq 1 18)
 do
 
   # 1 FST
-  vcffile_all=chr${chr}_merge.vcf.gz   # vcf data
-  $vcftools --gzvcf $vcffile_all --weir-fst-pop $case.txt --weir-fst-pop $control.txt --out ${group}_fst_chr$chr --fst-window-size 100000 --fst-window-step 10000
+  $vcftools --gzvcf $vcf --weir-fst-pop $case --weir-fst-pop $control --out ${group}_fst_chr$chr --fst-window-size 100000 --fst-window-step 10000
 
   # 2 assoc / allele frequence chi-square test
-  plink_data_out=chr${chr}_case_pheno  # plink data  # with phenotype
-  $plink --bfile $plink_data_out --double-id --allow-no-sex --assoc --adjust --out $plink_out
+  plink_data_out=chr${chr}_case_pheno 
+  $plink --bfile $plink_data_out --assoc --adjust --out $plink_out
   
 done
